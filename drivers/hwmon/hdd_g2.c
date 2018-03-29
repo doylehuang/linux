fb7b4667ec96edc52fccc02eb173add829624180
Doyle Huang <doyle.sy.huang@mail.foxconn.com>
2018-03-29 08:50:09 +0800

    (feat): add hdd g2 driver module
    hdd g2 driver module includes:
      - hdd micron 9200
      - hdd pm963
    
    Platform has only one hdd type which is hdd micron 9200 or
    is hdd pm963.
    
    Change-Id: I50ed807f99fa919ba44dca1beafd48fb360aa9a0
    Signed-off-by: Doyle Huang <doyle.sy.huang@mail.foxconn.com>

---
+++ drivers/hwmon/hdd_g2.c
@@ -0,0 +1,325 @@
+#include <linux/module.h>
+#include <linux/delay.h>
+#include <linux/init.h>
+#include <linux/i2c.h>
+#include <linux/hwmon.h>
+#include <linux/hwmon-sysfs.h>
+#include <linux/err.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+
+struct hdd_g2_data {
+	struct i2c_client	*client;
+	struct device		*hwmon_dev;
+	struct mutex		update_lock;
+	int			temperature;
+};
+
+#define HDD_MICRON_SLAVE_ADDR (0x53)
+#define HDD_MICRON_MUX_SELECTION (1)
+#define HDD_MICRON_VENDOR_ID (0x1344)
+#define HDD_PM963_SLAVE_ADDR (0x6a)
+#define HDD_PM963_I2C_RETRY (10)
+#define HDD_PM963_I2C_RETRY_DELAY_MS (10)
+
+
+struct mutex		inspect_update_lock;
+
+/**
+ * hdd_g2_i2c_access - Send to I2C Command register
+ */
+static s32 hdd_g2_i2c_access(struct device *dev, struct i2c_msg *hdd_i2c_msg, int hdd_i2c_msg_num)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+	struct i2c_client *client = data->client;
+	struct i2c_adapter *adapter = client->adapter;
+
+	if (i2c_transfer(adapter, hdd_i2c_msg, hdd_i2c_msg_num) < 0)
+		return -1;
+
+	return 0;
+}
+
+static int hdd_seletion(struct device *dev, u8 mux)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+	struct i2c_client *client = data->client;
+	struct i2c_msg hdd_i2c_msg[2] = {0};
+	int hdd_i2c_msg_num = 0;
+	u8 hdd_write_buf[] = {mux};
+
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = client->addr;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = 0;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_write_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_write_buf);
+	hdd_i2c_msg_num+=1;
+
+	return hdd_g2_i2c_access(dev, hdd_i2c_msg, hdd_i2c_msg_num);
+}
+
+static int hdd_deseletion(struct device *dev)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+	struct i2c_client *client = data->client;
+	struct i2c_msg hdd_i2c_msg[2] = {0};
+	int hdd_i2c_msg_num = 0;
+	u8 hdd_write_buf[] = {0x00};
+
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = client->addr;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = 0;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_write_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_write_buf);
+	hdd_i2c_msg_num+=1;
+
+	return hdd_g2_i2c_access(dev, hdd_i2c_msg, hdd_i2c_msg_num);
+}
+
+static int debug_print = 0;
+
+static int hdd_micron_get_temperature(struct device *dev, u8 type)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+	struct i2c_client *client = data->client;
+
+	struct i2c_msg hdd_i2c_msg[2] = {0};
+	int hdd_i2c_msg_num = 0;
+	u8 hdd_write_buf[] = {0x4d};
+	u8 hdd_read_buf[I2C_SMBUS_BLOCK_MAX] = {0};
+	int ret = 0;
+	u16 vendor_id = 0;
+
+	if (hdd_seletion(dev, HDD_MICRON_MUX_SELECTION) < 0)
+		goto micron_abort;
+
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = HDD_MICRON_SLAVE_ADDR;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = 0;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_write_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_write_buf);
+	hdd_i2c_msg_num+=1;
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = HDD_MICRON_SLAVE_ADDR;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = I2C_M_RD;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_read_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_read_buf);
+	hdd_i2c_msg_num+=1;
+
+	if (hdd_g2_i2c_access(dev, hdd_i2c_msg, hdd_i2c_msg_num) < 0)
+		goto micron_abort;
+
+	vendor_id = hdd_read_buf[4] + (hdd_read_buf[5] << 8);
+	if (vendor_id != HDD_MICRON_VENDOR_ID)
+		goto micron_abort;
+
+	ret = hdd_read_buf[8] + (hdd_read_buf[9] << 8);
+
+micron_abort:
+
+	hdd_deseletion(dev);
+	return ret;
+}
+
+static struct hdd_g2_data *hdd_micron_update_temperature(struct device *dev)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+
+	mutex_lock(&inspect_update_lock);
+	mutex_lock(&data->update_lock);
+
+	data->temperature = hdd_micron_get_temperature(dev, 0x00);
+
+	mutex_unlock(&data->update_lock);
+	mutex_unlock(&inspect_update_lock);
+	return data;
+}
+
+static ssize_t show_hdd_micron_temp(struct device *dev, struct device_attribute *da,
+			 char *buf)
+{
+	struct hdd_g2_data *data = hdd_micron_update_temperature(dev);
+
+	if (IS_ERR(data))
+		return PTR_ERR(data);
+
+	return sprintf(buf, "%d\n", data->temperature);
+}
+
+static int hdd_pm963_get_temperature(struct device *dev, ssize_t device_index)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+	struct i2c_client *client = data->client;
+
+	struct i2c_msg hdd_i2c_msg[2] = {0};
+	int hdd_i2c_msg_num = 0;
+	u8 hdd_write_buf[] = {0x00};
+	u8 hdd_read_buf[I2C_SMBUS_BLOCK_MAX] = {0};
+	int ret = 0;
+	u16 retry = 0;
+
+hdd_pm963_get_temperature_retry:
+
+	if (hdd_seletion(dev, (u8) (1 << device_index)) < 0)
+		goto pm963_abort;
+
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = HDD_PM963_SLAVE_ADDR;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = 0;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_write_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_write_buf);
+	hdd_i2c_msg_num+=1;
+	hdd_i2c_msg[hdd_i2c_msg_num].addr = HDD_PM963_SLAVE_ADDR;
+	hdd_i2c_msg[hdd_i2c_msg_num].flags = I2C_M_RD;
+	hdd_i2c_msg[hdd_i2c_msg_num].buf = hdd_read_buf;
+	hdd_i2c_msg[hdd_i2c_msg_num].len = sizeof(hdd_read_buf);
+	hdd_i2c_msg_num+=1;
+
+	if (hdd_g2_i2c_access(dev, hdd_i2c_msg, hdd_i2c_msg_num) < 0)
+		goto pm963_abort;
+
+	ret = hdd_read_buf[3];
+pm963_abort:
+
+	hdd_deseletion(dev);
+	if (ret == 0 && retry < HDD_PM963_I2C_RETRY)
+	{
+		mdelay(HDD_PM963_I2C_RETRY_DELAY_MS);
+		memset(hdd_i2c_msg, 0, sizeof(hdd_i2c_msg));
+		hdd_i2c_msg_num = 0;
+		retry+=1;
+		goto hdd_pm963_get_temperature_retry;
+	}
+
+	if (ret == 0xff) //no device
+		return 0;
+
+	return ret;
+}
+
+static struct hdd_g2_data *hdd_pm963_update_temperature(struct device *dev, ssize_t device_index)
+{
+	struct hdd_g2_data *data = dev_get_drvdata(dev);
+
+	if (device_index < 0) {
+		data->temperature = 0;
+		return data;
+	}
+
+	mutex_lock(&inspect_update_lock);
+	mutex_lock(&data->update_lock);
+
+	data->temperature = hdd_pm963_get_temperature(dev, device_index);
+
+	mutex_unlock(&data->update_lock);
+	mutex_unlock(&inspect_update_lock);
+	return data;
+}
+
+
+
+static ssize_t show_hdd_pm963_temp(struct device *dev, struct device_attribute *da,
+			 char *buf)
+{
+	struct hdd_g2_data *data = NULL;
+	ssize_t device_index = -1;
+
+	if (da != NULL && da->attr.name != NULL) {
+		sscanf(da->attr.name, "pm963_temp%d_input", &device_index);
+		device_index -= 1;
+	}
+
+	data = hdd_pm963_update_temperature(dev, device_index);
+
+	if (IS_ERR(data))
+		return PTR_ERR(data);
+
+	return sprintf(buf, "%d\n", data->temperature);
+}
+
+
+static SENSOR_DEVICE_ATTR(micron_temp1_input, S_IRUGO, show_hdd_micron_temp, NULL, 0);
+static SENSOR_DEVICE_ATTR(pm963_temp1_input, S_IRUGO, show_hdd_pm963_temp, NULL, 0);
+static SENSOR_DEVICE_ATTR(pm963_temp2_input, S_IRUGO, show_hdd_pm963_temp, NULL, 0);
+static SENSOR_DEVICE_ATTR(pm963_temp3_input, S_IRUGO, show_hdd_pm963_temp, NULL, 0);
+static SENSOR_DEVICE_ATTR(pm963_temp4_input, S_IRUGO, show_hdd_pm963_temp, NULL, 0);
+
+
+static struct attribute *hdd_g2_attrs[] = {
+	&sensor_dev_attr_micron_temp1_input.dev_attr.attr,
+	&sensor_dev_attr_pm963_temp1_input.dev_attr.attr,
+	&sensor_dev_attr_pm963_temp2_input.dev_attr.attr,
+	&sensor_dev_attr_pm963_temp3_input.dev_attr.attr,
+	&sensor_dev_attr_pm963_temp4_input.dev_attr.attr,
+	NULL
+};
+ATTRIBUTE_GROUPS(hdd_g2);
+
+static int
+hdd_g2_probe(struct i2c_client *client, const struct i2c_device_id *id)
+{
+	struct device *dev = &client->dev;
+	struct hdd_g2_data *data;
+
+	data = devm_kzalloc(dev, sizeof(struct hdd_g2_data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	data->client = client;
+	i2c_set_clientdata(client, data);
+	mutex_init(&data->update_lock);
+
+	mutex_init(&inspect_update_lock);
+
+	data->hwmon_dev = hwmon_device_register_with_groups(dev, client->name,
+								data,
+								hdd_g2_groups);
+	if (IS_ERR(data->hwmon_dev))
+		return PTR_ERR(data->hwmon_dev);
+
+	dev_info(dev, "%s: sensor '%s'\n",
+		 dev_name(data->hwmon_dev), client->name);
+
+	return 0;
+}
+
+static int hdd_g2_remove(struct i2c_client *client)
+{
+	struct hdd_g2_data *data = i2c_get_clientdata(client);
+
+	hwmon_device_unregister(data->hwmon_dev);
+	return 0;
+}
+
+static const struct i2c_device_id hdd_g2_ids[] = {
+	{ "hdd_g2", 0, },
+	{ /* LIST END */ }
+};
+MODULE_DEVICE_TABLE(i2c, hdd_g2_ids);
+
+static int hdd_g2_detect(struct i2c_client *new_client,
+				   struct i2c_board_info *info)
+{
+	/* NOTE we're assuming device described in DTS is present. */
+	struct i2c_adapter *adapter = new_client->adapter;
+
+	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BLOCK_DATA))
+		return -ENODEV;
+
+	strlcpy(info->type, "hdd_g2", I2C_NAME_SIZE);
+
+	return 0;
+}
+
+static struct i2c_driver hdd_g2_driver = {
+	.class		= I2C_CLASS_HWMON,
+	.driver = {
+		.name	= "hdd_g2",
+	},
+	.probe		= hdd_g2_probe,
+	.remove		= hdd_g2_remove,
+	.id_table	= hdd_g2_ids,
+	.detect		= hdd_g2_detect,
+	.address_list	= NULL,
+};
+
+module_i2c_driver(hdd_g2_driver);
+
+MODULE_AUTHOR("FXN-KS-BMC");
+MODULE_LICENSE("GPL");
+
